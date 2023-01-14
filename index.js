737067bf1b6c6436401b271101c38eb298fbd730
jannat456nur <jannatjenny22@gmail.com>
2023-01-14 09:42:15 +0600

    first

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const express = require("express")
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.69qz5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db('doctorportal');
+        const appointmentOptionCollection = database.collection('appointmentOptions');
+        
+        console.log('Connected correctly to server');
+        
+        // get all appointment
+        app.get('/appointmentOptions', async(req, res) => {
+            const cursor = appointmentOptionCollection.find({});
+            const appointment = await cursor.toArray();
+            res.send(appointment)
+            console.log('hit jairam gi')
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(` app listening at http://localhost:${port}`)
+})
